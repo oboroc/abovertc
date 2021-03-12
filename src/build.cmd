c6e79971b44e99ff49f6e6766978c0580d407f24
Adrian Oboroc <oboroc@users.noreply.github.com>
2021-03-12 08:26:29 -0500

    Update build.cmd

--- src/build.cmd
+++ src/build.cmd
@@ -4,4 +4,4 @@ del *.LST
 del *.ref
 del *.err
 "C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe" indos.bat -noautoexec -exit
-fc /b 3rdparty\CLOCK.SYS MASM51.SYS > compare.err
+fc /b ..\3rdparty\CLOCK.SYS MASM51.SYS > compare.err
