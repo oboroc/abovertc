14a0b57438c341fbcad0ffaa197cd353bd7f6bb6
Adrian Oboroc <oboroc@users.noreply.github.com>
2023-05-05 09:58:58 -0400

    Cleanup build batch files

--- src/build.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-del *.SYS
-del *.LST
-del *.ref
-del *.err
-"C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe" indos.bat -noautoexec -exit
-fc /b ..\3rdparty\CLOCK.SYS MASM51.SYS > compare.err
